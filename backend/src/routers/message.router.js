fd8dfaa6402263d2a1337a7755fb51e73b379446
Rohit Singh Negi <37205344+Rohit8BitCoder@users.noreply.github.com>
2025-03-05 11:19:33 +0530

    Update message.router.js

--- backend/src/routers/message.router.js
+++ backend/src/routers/message.router.js
@@ -1,13 +1,11 @@
-import express from 'express'
-import {protectRoute} from "../middleware/auth.middleware.js"
-import {getUserForSidebar} from '../controllers/message.controller.js'
+import express from 'express';
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getUsersForSidebar, getMessages, sendMessage } from '../controllers/message.controller.js';
 
 const router = express.Router();
 
-router.get('/users',protectRoute,getUserForSidebar);
-router.get('./:id',protectRoute,getMessages);
-
-router.post("./send/:id", protectRoute, sendMessages);
-
+router.get('/users', protectRoute, getUsersForSidebar);
+router.get('/:id', protectRoute, getMessages);
+router.post('/send/:id', protectRoute, sendMessage);
 
 export default router;
