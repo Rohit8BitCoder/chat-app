079623c995ba198a69e5e7b7c9a77d99732916b0
Rohit Singh Negi <37205344+Rohit8BitCoder@users.noreply.github.com>
2025-03-05 11:11:04 +0530

    Update cloudinary.js

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -1,11 +1,10 @@
 import { v2 as cloudinary } from 'cloudinary';
+import { config } from 'dotenv';
 
-import {config} from "dotenv";
-
-config();
+config(); // Load environment variables
 
 cloudinary.config({
-  cloud_name:process.enc.CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
